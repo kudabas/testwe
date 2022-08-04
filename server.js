23ecd98a25a39295db550e28f5fde96e1fa866fe
aria <ariakh177@gmail.com>
2022-08-04 17:54:50 +0700

    fix server

---
+++ server.js
@@ -0,0 +1,12 @@
+const express=require("express")
+const serveStatic=require("serve-static")
+const path=require("path")
+
+const app=express()
+
+app.use("/", serveStatic(path.join(__dirname, "/dist")))
+
+const port=8000
+app.listen(port)
+
+console.log("aplikasi berjalan di port"+port)
