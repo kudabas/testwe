75ec37aaa816e39634f58fe973e8cf26b9f47b03
aria <ariakh177@gmail.com>
2022-08-04 18:12:29 +0700

    wakwaw

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app=express()
 
 app.use("/", serveStatic(path.join(__dirname, "/dist")))
 
-const port=8000
+const port=process.env.port || 8000
 app.listen(port)
 
 console.log("aplikasi berjalan di port"+port)
